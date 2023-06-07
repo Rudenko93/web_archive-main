19cec5868a2e34a247c65db08998a884d375945c
Rudenko93 <dv04111993@yandex.ru>
2023-06-07 11:22:28 +0200

    button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,10 +1,11 @@
 import clsx from "clsx"
 import { DOMAttributes } from "react"
+import "./Button.scss"
 
 interface IButtonProps extends DOMAttributes<HTMLButtonElement> {
-  isDisabled: boolean
-  typeButton: "button" | "submit" | "reset"
-  onClick: (event: React.MouseEvent) => void
+  isDisabled?: boolean
+  typeButton?: "button" | "submit" | "reset"
+  onClick?: (event: React.MouseEvent) => void
   className?: string
 }
 
@@ -21,7 +22,7 @@ export const Button: React.FC<IButtonProps> = ({
       onClick={onClick}
       disabled={isDisabled}
       className={clsx("Button", className, {
-        Button__disabled: isDisabled,
+        Button_disabled: isDisabled,
       })}>
       <span>{children}</span>
     </button>
--- src/pages/ButtonPage/ButtonPage.tsx
+++ src/pages/ButtonPage/ButtonPage.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { ETypographyVariant, Typography } from "../../components/Typography"
 import "./ButtonPage.scss"
+import { Button } from "../../components/Button"
 
 export const ButtonPage: React.FC = () => {
   return (
@@ -8,6 +9,10 @@ export const ButtonPage: React.FC = () => {
       <Typography as="h1" variant={ETypographyVariant.TextH1Bold}>
         Button
       </Typography>
+      <Button>Click</Button>
+      <br />
+      <br />
+      <Button isDisabled>Disabled</Button>
     </div>
   )
 }
