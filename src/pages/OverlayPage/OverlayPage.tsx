59efe3131b5595aacf84302eb54cc25144fa6127
Deni <dv04111993@yandex.ru>
2023-06-20 19:57:54 +0200

    overlay ui

--- src/pages/OverlayPage/OverlayPage.tsx
+++ src/pages/OverlayPage/OverlayPage.tsx
@@ -1,12 +1,26 @@
-import { ETypographyVariant, Typography } from "ui"
+import { useState } from "react"
+import { Button, ETypographyVariant, Overlay, Typography } from "ui"
 import "./OverlayPage.scss"
 
-export const OverlayPage = () => {
+export const OverlayPage: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleOpen = () => {
+    setIsOpen(true)
+  }
+
+  const handleClose = () => {
+    setIsOpen(false)
+  }
+
   return (
     <section className="OverlayPage">
       <Typography as="h1" variant={ETypographyVariant.TextH1Bold}>
         Overlay
       </Typography>
+
+      <Button onClick={handleOpen}>Open</Button>
+      <Overlay isActive={isOpen} onClick={handleClose} />
     </section>
   )
 }
---
+++ src/ui/Overlay/Overlay.tsx
@@ -0,0 +1,36 @@
+import { memo, useRef } from "react"
+import type { FC, MouseEvent } from "react"
+import { CSSTransition } from "react-transition-group"
+import classNames from "classnames"
+
+import "./Overlay.scss"
+
+type TProps = {
+  className?: string
+  isActive?: boolean
+  onClick?: (event: MouseEvent) => void
+  timeout?: number
+}
+
+const Component: FC<TProps> = ({
+  className,
+  isActive = false,
+  onClick,
+  timeout = 300,
+}) => {
+  const nodeRef = useRef(null)
+
+  return (
+    <CSSTransition
+      className={classNames("Overlay", className)}
+      in={isActive}
+      nodeRef={nodeRef}
+      onClick={onClick}
+      timeout={timeout}
+      unmountOnExit>
+      <div ref={nodeRef} />
+    </CSSTransition>
+  )
+}
+
+export const Overlay = memo(Component)
