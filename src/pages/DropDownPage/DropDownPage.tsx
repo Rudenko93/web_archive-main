7cd1fa7c8cbc2b9b40d29634ae992fb9a1888cc8
Rudenko93 <dv04111993@yandex.ru>
2023-08-18 12:37:36 +0200

    mobile design changes

--- src/pages/DropDownPage/DropDownPage.tsx
+++ src/pages/DropDownPage/DropDownPage.tsx
@@ -7,30 +7,30 @@ export const DropDownPage: React.FC = () => {
       <Typography as="h1" variant={ETypographyVariant.TextH1Bold}>
         DropDown
       </Typography>
-      <nav className="DropDownPage__Navigation">
-        <ul className="DropDownPage__Menu">
+      <nav className="DropDownPage__navigation">
+        <ul className="DropDownPage__menu">
           <DropDown
             classes={{
-              title: "DropDownPage__MenuItem",
+              title: "DropDownPage__menuItem",
               list: "DropDownCatalog",
             }}
             title="Smartphones">
-            <ul className="DropDownCatalog__Menu">
-              <li className="DropDownCatalog__MenuItem">Apple</li>
-              <li className="DropDownCatalog__MenuItem">Samsung</li>
-              <li className="DropDownCatalog__MenuItem">Google</li>
+            <ul className="DropDownCatalog__menu">
+              <li className="DropDownCatalog__menuItem">Apple</li>
+              <li className="DropDownCatalog__menuItem">Samsung</li>
+              <li className="DropDownCatalog__menuItem">Google</li>
             </ul>
           </DropDown>
           <DropDown
             classes={{
-              title: "DropDownPage__MenuItem",
+              title: "DropDownPage__menuItem",
               list: "DropDownCatalog",
             }}
             title="Tablets">
-            <ul className="DropDownCatalog__Menu">
-              <li className="DropDownCatalog__MenuItem">Apple</li>
-              <li className="DropDownCatalog__MenuItem">Samsung</li>
-              <li className="DropDownCatalog__MenuItem">Google</li>
+            <ul className="DropDownCatalog__menu">
+              <li className="DropDownCatalog__menuItem">Apple</li>
+              <li className="DropDownCatalog__menuItem">Samsung</li>
+              <li className="DropDownCatalog__menuItem">Google</li>
             </ul>
           </DropDown>
         </ul>
